3f178a4bf5d8bc4e5ea9911e5a498ec10e3c5666
Alejandro <78627273+solanina@users.noreply.github.com>
2021-05-19 00:33:09 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ if (horas >= 6 && horas < 12){
 
 }
 
-if (horas >= 12 && horas < 20) {
+if (horas >= 12 && horas <= 19) {
 document.getElementsByTagName("h1")[0].innerHTML = "점심 드셨어요~";
 document.getElementsByTagName("h1")[0].style.color = "#ead3cb";
 document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #bdc7c9";
@@ -47,7 +47,7 @@ for (var i = 0; i < elementos.length; i++) {
 
 }
 
-if (horas > 20) {
+if (horas >= 20) {
 document.getElementsByTagName("h1")[0].innerHTML = "안녕히 주무세요~";
 document.getElementsByTagName("h1")[0].style.color = "#f3bda1";
 document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #687980";
@@ -65,8 +65,25 @@ for (var i = 0; i < elementos.length; i++) {
     elementos[i].style.color = "#687980";
 }
 
-}
+} else {
+  document.getElementsByTagName("h1")[0].innerHTML = "안녕히 주무세요~";
+  document.getElementsByTagName("h1")[0].style.color = "#f3bda1";
+  document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #687980";
+  document.getElementsByTagName("body")[0].style.backgroundColor = "#02475e";
+  document.querySelectorAll("img")[0].src = "image/chuu2.png";
 
+
+  var elements = document.querySelectorAll(".link");
+  for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "#f3bda1";
+  }
+
+  var elementos = document.querySelectorAll(".paragraph");
+  for (var i = 0; i < elementos.length; i++) {
+      elementos[i].style.color = "#687980";
+  }
+
+  }
 }
 
 
