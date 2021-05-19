18228782bcb2ac332d1510d28acbca0d03a3acf8
Alejandro <78627273+solanina@users.noreply.github.com>
2021-05-19 20:24:49 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,27 @@
-var date = new Date();
-var horas = date.getHours();
-
-
 function timer() {
+  var date = new Date();
+   var hora = date.getHours();
+////////////////////////////*****************MAÑANA***************************
 
-if (horas >= 6 && horas < 12){
+if (hora >= 6 && hora < 12) {
   document.getElementsByTagName("h1")[0].innerHTML = "좋은 아침~";
   document.getElementsByTagName("h1")[0].style.color = "#ff7b54";
   document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #ffd56b";
   document.getElementsByTagName("body")[0].style.backgroundColor = "#939b62";
   document.querySelectorAll("img")[0].src = "image/chuu.png";
 
-
   var elements = document.querySelectorAll(".link");
   for (var i = 0; i < elements.length; i++) {
       elements[i].style.color = "#ffd56b";
   }
-
   var elementos = document.querySelectorAll(".paragraph");
   for (var i = 0; i < elementos.length; i++) {
       elementos[i].style.color = "#ff7b54";
   }
-
-
 }
+///////////////////////////*****************TARDE***************************
 
-if (horas >= 12 && horas <= 19) {
+if (hora >= 12 && hora < 19) {
 document.getElementsByTagName("h1")[0].innerHTML = "점심 드셨어요~";
 document.getElementsByTagName("h1")[0].style.color = "#ead3cb";
 document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #bdc7c9";
@@ -42,12 +38,13 @@ var elementos = document.querySelectorAll(".paragraph");
 for (var i = 0; i < elementos.length; i++) {
     elementos[i].style.color = "#bdc7c9";
 }
-
-
-
 }
 
-if (horas >= 20) {
+
+////////////////////////*****************NOCHE***************************
+
+ if (hora >= 19 && hora < 23 || hora >= 0 && hora < 6){
+
 document.getElementsByTagName("h1")[0].innerHTML = "안녕히 주무세요~";
 document.getElementsByTagName("h1")[0].style.color = "#f3bda1";
 document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #687980";
@@ -63,28 +60,8 @@ for (var i = 0; i < elements.length; i++) {
 var elementos = document.querySelectorAll(".paragraph");
 for (var i = 0; i < elementos.length; i++) {
     elementos[i].style.color = "#687980";
+
 }
-
-} else {
-  document.getElementsByTagName("h1")[0].innerHTML = "안녕히 주무세요~";
-  document.getElementsByTagName("h1")[0].style.color = "#f3bda1";
-  document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #687980";
-  document.getElementsByTagName("body")[0].style.backgroundColor = "#02475e";
-  document.querySelectorAll("img")[0].src = "image/chuu2.png";
-
-
-  var elements = document.querySelectorAll(".link");
-  for (var i = 0; i < elements.length; i++) {
-      elements[i].style.color = "#f3bda1";
-  }
-
-  var elementos = document.querySelectorAll(".paragraph");
-  for (var i = 0; i < elementos.length; i++) {
-      elementos[i].style.color = "#687980";
-  }
-
-  }
 }
-
-
+}
 timer();
