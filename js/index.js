7b6baf9ede6ac1bd6541bb1483938c278a4dbecd
Alejandro <78627273+solanina@users.noreply.github.com>
2021-05-22 20:27:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function timer() {
   var date = new Date();
-   var hora = date.getHours();
+    var hora = date.getHours();
 ////////////////////////////*****************MAÑANA***************************
 
 if (hora >= 6 && hora < 12) {
@@ -9,6 +9,8 @@ if (hora >= 6 && hora < 12) {
   document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #ffd56b";
   document.getElementsByTagName("body")[0].style.backgroundColor = "#939b62";
   document.querySelectorAll("img")[0].src = "image/chuu.png";
+  document.querySelectorAll("link")[5].href = "image/favicon/naranja.png";
+  document.querySelectorAll("title")[0].innerHTML = "좋은 아침~";
 
   var elements = document.querySelectorAll(".link");
   for (var i = 0; i < elements.length; i++) {
@@ -27,7 +29,8 @@ document.getElementsByTagName("h1")[0].style.color = "#ead3cb";
 document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #bdc7c9";
 document.getElementsByTagName("body")[0].style.backgroundColor = "#845460";
 document.querySelectorAll("img")[0].src = "image/chuu3.png";
-
+document.querySelectorAll("link")[5].href = "image/favicon/riceball.png";
+document.querySelectorAll("title")[0].innerHTML = "점심 드셨어요~";
 
 var elements = document.querySelectorAll(".link");
 for (var i = 0; i < elements.length; i++) {
@@ -50,6 +53,8 @@ document.getElementsByTagName("h1")[0].style.color = "#f3bda1";
 document.getElementsByTagName("h1")[0].style.textShadow = "2px 2px #687980";
 document.getElementsByTagName("body")[0].style.backgroundColor = "#02475e";
 document.querySelectorAll("img")[0].src = "image/chuu2.png";
+document.querySelectorAll("link")[5].href = "image/favicon/estrella.png";
+document.querySelectorAll("title")[0].innerHTML = "안녕히 주무세요~";
 
 
 var elements = document.querySelectorAll(".link");
